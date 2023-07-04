ff863fe9664fcab1d7d8d655a7587fbd9323d735
Trivial-lore <triviallore@gmail.com>
2023-07-03 20:55:05 -0500

    b

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,19 @@
 class QuizBrain:
-
+  def __init__(self, list_of_questions):
+    self.question_list = list_of_questions
+    self.score=0
+    self.question_count=0
+  
+  def get_question(self):
+    question = self.question_list[question_count]
+    if question:
+      print(question.question)
+    else:
+      print("No question found.")
+  
+  def get_answer(self):
+    answer= self.question_list[question_count]
+    
+  
+  
   
