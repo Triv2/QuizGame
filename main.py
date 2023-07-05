8cba32f55525d43160d02d62b57ec3a36619c47f
Trivial-lore <triviallore@gmail.com>
2023-07-05 09:34:10 -0500

    finished

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from quiz_brain import QuizBrain
 
 question_bank=[]
 for question in question_data:
-  question_text=question.data.question["question"]
-  question_answer=question.data.answer["correct_answer"]
+  question_text=question["question"]
+  question_answer=question["correct_answer"]
   new_question=Question(question_text,question_answer)
   question_bank.append(new_question)
   
+quiz_brain=QuizBrain(question_bank)
   
-  
-for question in question_bank:
-   
-  
-  
-  
+def main(): 
+  if quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+    main()
+  else:
+    print("Quiz Complete!")
+    print(f"Your score is {quiz_brain.score}/{quiz_brain.question_count}")
+      
+main()
--- quiz_brain.py
+++ quiz_brain.py
@@ -11,12 +11,30 @@ class QuizBrain:
     else:
       print("No question found.")
   
-  def get_answer(self):
-    answer= self.question_list[self.question_count]
-    if answer:
-      print(answer)
+  def check_answer(self, user_answer, correct_answer):
+    valid_user_answer = user_answer.lower()
+    valid_correct_answer = correct_answer.lower()
+    if valid_user_answer==valid_correct_answer:
+      print("Correct!")
+      self.score+=1
     else:
-      print("No answer found.")
+      print("Wrong!")
+    print(f"The correct answer is {correct_answer}")
+    print(f"Your score is {self.score}/{self.question_count}\n")
+      
+  def still_has_questions(self):
+    length = len(self.question_list)
+    if self.question_count < length:
+      return True
+    else:
+      return False
+  
+  def next_question(self):
+    current_question = self.question_list[self.question_count]
+    self.question_count+=1
+    user_answer = input(f"Q.{self.question_count}: {current_question.question} (True/False): ")
+    self.check_answer(user_answer, current_question.answer)
+    
     
   
   
